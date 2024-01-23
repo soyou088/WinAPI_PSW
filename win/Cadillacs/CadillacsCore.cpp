8f45b7c63703de2ede61684bcacf311ccc2ba60b
soyou088 박성우 <soyou088@gmail.com>
2024-01-23 15:25:57 +0900

    박성우 : EngineStart

---
+++ win/Cadillacs/CadillacsCore.cpp
@@ -0,0 +1,29 @@
+#include "CadillacsCore.h"
+
+CadillacsCore::CadillacsCore()
+{
+}
+
+CadillacsCore::~CadillacsCore()
+{
+}
+
+void CadillacsCore::EngineStart()
+{
+	//MainWindow.SetTitle();
+	//MainWindow.SetScale();
+
+
+	// 이때되면 이미 윈도우 창은 만들어져있는 상태일거라고 
+	int a = 0;
+}
+
+void CadillacsCore::EngineUpdate()
+{
+	int a = 0;
+}
+
+void CadillacsCore::EngineEnd()
+{
+	int a = 0;
+}
---
+++ win/Cadillacs/CadillacsCore.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <EngineCore\EngineCore.h>
+
+// Ό³Έν :
+class CadillacsCore : public EngineCore
+{
+public:
+	// constrcuter destructer
+	CadillacsCore();
+	~CadillacsCore();
+
+	// delete Function
+	CadillacsCore(const CadillacsCore& _Other) = delete;
+	CadillacsCore(CadillacsCore&& _Other) noexcept = delete;
+	CadillacsCore& operator=(const CadillacsCore& _Other) = delete;
+	CadillacsCore& operator=(CadillacsCore&& _Other) noexcept = delete;
+
+protected:
+	void EngineStart() override;
+	void EngineUpdate() override;
+	void EngineEnd() override;
+
+private:
+
+};
+
